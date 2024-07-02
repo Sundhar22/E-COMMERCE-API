232c26b2d775399f104d59e6c33746013747eedd
Indhiraraj <indhiraraj7@gmail.com>
2024-07-02 20:31:46 +0530

    Handle MethodArgumentNotValidException

--- src/main/java/com/example/ecommerce/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/ecommerce/exceptions/GlobalExceptionHandler.java
@@ -5,12 +5,15 @@ import org.springframework.http.ProblemDetail;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AccountStatusException;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.security.SignatureException;
+
+import java.util.Optional;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -27,9 +30,18 @@ public class GlobalExceptionHandler {
 
             return errorDetail;
         }
-        if (exception instanceof MethodArgumentNotValidException){
-            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(400),exception.getMessage());
-            errorDetail.setProperty("description","The password is invalid");
+        if (exception instanceof MethodArgumentNotValidException ex){
+            String specificMessage = ex.getBindingResult().getAllErrors().stream()
+                    .filter(objectError -> objectError instanceof FieldError)
+                    .map(objectError -> ((FieldError) objectError))
+                    .map(fieldError -> Optional.ofNullable(fieldError.getDefaultMessage()).orElse("Validation failed"))
+                    .findFirst()
+                    .orElse("Validation error occurred");
+
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(400), specificMessage);
+            errorDetail.setProperty("description", "The password is invalid");
+            errorDetail.setDetail(specificMessage);
+            return errorDetail;
         }
 
         if (exception instanceof AccountStatusException) {
